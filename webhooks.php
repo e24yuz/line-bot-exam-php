3aa1d94e2196d3d696c252c3742b3248f7f274aa
e24yuz <49187848+e24yuz@users.noreply.github.com>
2019-04-02 16:13:12 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'kLAIEUS+KbToHB+cqbrdJFoY4r0sDMpoduaN5ZQhWoz58CN69O1wnoKvly/Dq5o0uUnzLWn6tUc1QpX818bD63lwYObX2QZ/pHcC1PIBK2rvDasY1RfNPF59g+mSAEkf0sPhDswCkNMzRf11VpZklQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
